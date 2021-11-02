fabb9402b956850357fb28c1cb041015659e02d4
Cyprian Kyalo <cypkyalo17@gmail.com>
2021-11-02 22:45:34 +0300

    Window Size

--- main.cpp
+++ main.cpp
@@ -124,7 +124,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
 
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(1000, 500);
+	glutInitWindowSize(1200, 700);
 	glutCreateWindow("Rectangle");
 
 	glClearColor(0.0, 0.0, 0.0, 0.0);         // black background
