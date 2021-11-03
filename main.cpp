b4ad895de8071c0277b3caec9010c6e2f2a59d71
Cyprian Kyalo <cypkyalo17@gmail.com>
2021-11-03 11:10:56 +0300

    Final Commit

--- main.cpp
+++ main.cpp
@@ -4,37 +4,82 @@
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);  // sets the bitplane area of the window to values previously selected by glClearColor
-	//glColor3f(0.37f, 0.22f, 0.07f); // Defines the fill color
-	glColor3f(0.1, 0.5, 0.0);
-	glBegin(GL_POLYGON); // Defines the type of object you want to draw
-	//
 
-	//House
-	glVertex2i(2, 2);
-	glVertex2i(6, 2);
-	glVertex2i(6, 6);
-	glVertex2i(2, 6);
+	//House Structure
+	glColor3f(0.0, 0.0, 1.0);
+	glLineWidth(1);
+	glBegin(GL_LINES);
+	glVertex2d(2.0, 6.0);
+	glVertex2d(2.0, 2.0);
+	glVertex2d(6.0, 2.0);
+	glVertex2d(6.0, 6.0);
+
+	glVertex2d(2.0, 2.0);
+	glVertex2d(6.0, 2.0);
+
+	glVertex2d(1.5, 6.0);
+	glVertex2d(2.0, 6.0);
+
+	glVertex2d(1.5, 6.0);
+	glVertex2d(1.5, 7.0);
+
+	glVertex2d(1.5, 7.0);
+	glVertex2d(6.5, 7.0);
+
+	glVertex2d(6.5, 7.0);
+	glVertex2d(6.5, 6.0);
+
+	glVertex2d(6.5, 6.0);
+	glVertex2d(6.0, 6.0);
+
 	glEnd();
 
-	glBegin(GL_POLYGON);
-	glVertex2d(1.5, 6);
-	glVertex2d(6.5, 6);
-	glVertex2d(6.5, 7);
-	glVertex2d(1.5, 7);
-	glEnd();
+	// Windows and Door
+	glColor3f(0.0, 1.0, 0.0);
+	glLineWidth(1);
+	glBegin(GL_LINES);
 
-	// Left Window
-	glColor3f(0.3, 0.5, 0.8);
-	glBegin(GL_POLYGON);
+	//Left Window
 	glVertex2d(2.3, 3.9);
 	glVertex2d(3.2, 3.9);
+
+	glVertex2d(3.2, 3.9);
+	glVertex2d(3.2, 4.9);
+
 	glVertex2d(3.2, 4.9);
 	glVertex2d(2.3, 4.9);
+
+	glVertex2d(2.3, 4.9);
+	glVertex2d(2.3, 3.9);
+
+	//Right Window
+	glVertex2d(4.8, 3.9);
+	glVertex2d(5.7, 3.9);
+
+	glVertex2d(5.7, 3.9);
+	glVertex2d(5.7, 4.9);
+
+	glVertex2d(5.7, 4.9);
+	glVertex2d(4.8, 4.9);
+
+	glVertex2d(4.8, 4.9);
+	glVertex2d(4.8, 3.9);
+
+	//Door
+	glVertex2d(4.5, 2.0);
+	glVertex2d(4.5, 5.0);
+
+	glVertex2d(4.5, 5.0);
+	glVertex2d(3.5, 5.0);
+
+	glVertex2d(3.5, 5.0);
+	glVertex2d(3.5, 2.0);
+
 	glEnd();
 
 	// Left Window grids
-	glColor3f(0.6, 0.2, 0.3);
-	glLineWidth(3);
+	glColor3f(0.0, 0.0, 1.0);
+	glLineWidth(1);
 	glBegin(GL_LINES);
 	glVertex2d(2.3, 4.4);
 	glVertex2d(3.2, 4.4);
@@ -42,27 +87,10 @@ void display(void)
 	glVertex2d(2.75, 4.9);
 	glEnd();
 
-	// Door
-	glColor3f(0.3, 0.5, 0.8);
-	glBegin(GL_POLYGON);
-	glVertex2d(3.5, 2);
-	glVertex2d(4.5, 2);
-	glVertex2d(4.5, 5);
-	glVertex2d(3.5, 5);
-	glEnd();
-
-	// Right Window
-	glColor3f(0.3, 0.5, 0.8);
-	glBegin(GL_POLYGON);
-	glVertex2d(4.8, 3.9);
-	glVertex2d(5.7, 3.9);
-	glVertex2d(5.7, 4.9);
-	glVertex2d(4.8, 4.9);
-	glEnd();
-
 	// Right Window grids
-	glColor3f(0.6, 0.2, 0.3);
+	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_LINES);
+	glLineWidth(1);
 	glVertex2d(4.8, 4.4);
 	glVertex2d(5.7, 4.4);
 	glVertex2d(5.25, 3.9);
@@ -83,7 +111,7 @@ void display(void)
 
 
 	// Second  Circle
-	glColor3f(0, 0, 0);  //black background
+	glColor3f(1, 1, 1);  //white background
 	float p = 0.0f;
 	float q = 0.35f;
 	float z = 9.45f;
@@ -129,7 +157,7 @@ int main(int argc, char** argv)
 	glutInitWindowSize(1200, 700);
 	glutCreateWindow("Rectangle");
 
-	glClearColor(0.0, 0.0, 0.0, 0.0);         // black background
+	glClearColor(1.0, 1.0, 1.0, 1.0);         // white background
 	glMatrixMode(GL_PROJECTION);              // sets the viewing projection, properties of the camera that views the object
 	glLoadIdentity();                           // start with identity matrix
 	glOrtho(0.0, 10.0, 0.0, 10.0, -1.0, 1.0);   // setting the coordinate system
